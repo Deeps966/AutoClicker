61a17c0ad977724035dba813821a3ffc5604a16f
Deep Shah <shahdeep966@gmail.com>
2021-12-20 11:07:54 +0530

    Code Simplified and for multiple clicks

--- autoClicker.py
+++ autoClicker.py
@@ -1,30 +1,30 @@
-# importing time and threading
 import time
 import threading
-from pynput.mouse import Button, Controller 
-
-# pynput.keyboard is used to watch events of
-# keyboard for start and stop of auto-clicker
+from pynput.mouse import Button, Controller
 from pynput.keyboard import KeyCode
 from pynput.mouse import Listener as MouseListener
 from pynput.keyboard import Listener as KeyboardListener
 
-# four variables are created to
-# control the auto-clicker
-delay = 1
-button = Button.left
-start_stop_key = KeyCode(char='a')
-stop_key = KeyCode(char='b')
-
 # threading.Thread is used
-# to control clicks
 class ClickMouse(threading.Thread):
     def __init__(self, delay, button):
         super(ClickMouse, self).__init__()
+        self.start_self.stop_key = KeyCode(char='a')
+        self.stop_key = KeyCode(char='b')
         self.delay = delay
         self.button = button
         self.running = False
         self.program_running = True
+
+        self.keyboard_listener = KeyboardListener(on_press=self.on_press)
+        self.mouse_listener = MouseListener(on_click=self.on_click)
+
+        self.keyboard_listener.start()
+        self.mouse_listener.start()
+        self.mouse = Controller()
+        self.mouseClicks = []
+        # self.keyboard_listener.join()
+        # self.mouse_listener.join()
 
     def start_clicking(self):
         self.running = True
@@ -40,74 +40,29 @@ class ClickMouse(threading.Thread):
         count = 0
         while self.program_running:
             while self.running:
-                for clickPos in mouseClicks:
-                    mouse.position = clickPos
-                    mouse.click(self.button)
-                    if mouseClicks == mouseClicks.__len__:
+                for clickPos in self.mouseClicks:
+                    self.mouse.position = clickPos
+                    self.mouse.click(self.button)
+                    print(clickPos)
+                    if clickPos == self.mouseClicks[self.mouseClicks.__len__() - 1]:
                         count = 0
                     else:
                         count = count + 1
-                        print(count)
-                # if count == 0:
-                #     mouse.position = mouseClicks[0]
-                #     mouse.click(self.button)
-                #     count = count + 1
-                # elif count == 1:
-                #     mouse.position = mouseClicks[1]
-                #     mouse.click(self.button)
-                #     count = count + 1
-                # elif count == 2:
-                #     mouse.position = mouseClicks[2]
-                #     mouse.click(self.button)
-                #     count = count + 1
-                #     time.sleep(1)
-                # elif count == 3:
-                #     mouse.position = mouseClicks[3]
-                #     mouse.click(self.button)
-                #     count = 0
-                time.sleep(self.delay)
-                time.sleep(0.1)
+                    time.sleep(self.delay)
+                    time.sleep(1)
 
-def on_click(x, y, button, pressed):
-    if pressed:
-        # print(x,y)
-        mouseClicks.append((x,y))
+    def on_click(self,x, y, button, pressed):
+        if pressed:
+            self.mouseClicks.append((x,y))
 
-# on_press method takes
-# key as argument
-def on_press(key):
-	
-# start_stop_key will stop clicking
-# if running flag is set to true
-	if key == start_stop_key:
-		if click_thread.running:
-			click_thread.stop_clicking()
-		else:
-			click_thread.start_clicking()
-			
-	# here exit method is called and when
-	# key is pressed it terminates auto clicker
-	elif key == stop_key:
-		click_thread.exit()
-		keyboard_listener.stop()
-		mouse_listener.stop()
+    def on_press(self,key):
+        if key == self.start_self.stop_key:
+            if click_thread.running:
+                click_thread.stop_clicking()
+            else:
+                click_thread.start_clicking()
+        elif key == self.stop_key:
+            click_thread.exit()
 
-# with KeyCodeListener(on_press=on_press) as listener:
-# 	listener.join()
-
-# with MouseListener(on_click=on_click) as ls:
-#     ls.join()
-
-keyboard_listener = KeyboardListener(on_press=on_press)
-mouse_listener = MouseListener(on_click=on_click)
-
-keyboard_listener.start()
-mouse_listener.start()
-# keyboard_listener.join()
-# mouse_listener.join()
-
-# instance of mouse controller is created
-mouse = Controller()
-click_thread = ClickMouse(delay, button)
+click_thread = ClickMouse(1, Button.left)
 click_thread.start()
-mouseClicks = []
